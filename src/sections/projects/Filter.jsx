74e9364bf38dd27cf06509d4736ed4d3ba60fa48
Casa <clara.gomez@balterra.com>
2025-09-03 11:39:22 +0200

    proyectos hecha
    simulada

--- src/sections/projects/Filter.jsx
+++ src/sections/projects/Filter.jsx
@@ -1,40 +1,44 @@
-import { t } from "i18next";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-t;
+import useEmblaCarousel from "embla-carousel-react";
+
 const Filter = () => {
   const { t } = useTranslation("projects");
+  const [emblaRef] = useEmblaCarousel({
+    dragFree: true,
+    containScroll: "trimSnaps",
+  });
 
   return (
-    <div>
-      <div className="flex justify-center gap-6">
+    <div className="overflow-hidden" ref={emblaRef}>
+      <div className="flex gap-6">
         <Link
-          to="{to}"
-          className="bg-coyote  text-alabaster rounded-4xl text-xl px-6 py-3 w-fit"
+          to=""
+          className="bg-coyote text-alabaster rounded-4xl text-xl px-6 py-3 w-fit flex-shrink-0"
         >
           {t("filter.all")}
         </Link>
         <Link
-          to="{to}"
-          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit"
+          to=""
+          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit flex-shrink-0"
         >
           {t("filter.flats")}
         </Link>
         <Link
-          to="{to}"
-          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit"
+          to=""
+          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit flex-shrink-0"
         >
           {t("filter.houses")}
-        </Link>{" "}
+        </Link>
         <Link
-          to="{to}"
-          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit"
+          to=""
+          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit flex-shrink-0"
         >
           {t("filter.empty")}
         </Link>
         <Link
-          to="{to}"
-          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit"
+          to=""
+          className="bg-alabaster border-coyote/70 border-2 text-coyote rounded-4xl text-xl px-6 py-3 w-fit flex-shrink-0"
         >
           {t("filter.furnished")}
         </Link>
--- src/sections/projects/GallerySection.jsx
+++ src/sections/projects/GallerySection.jsx
@@ -9,18 +9,27 @@ const GallerySection = () => {
       {/* filter */}
       <Filter />
 
-      {/* GallerySection */}
-      <div>
-        <div id="container" className="flex flex-col lg:flex-row gap-6 pt-12">
-          hola
-          {projects.map((project) => {
-            <div className="w-full lg:flex-1 h-120 rounded-2xl flex flex-col gap-3 justify-between">
-              <div className="bg-coyote flex-1 rounded-2xl">
-                <img src={project.image} alt="" />
+      {/* Gallery */}
+      <div className="pt-12">
+        <div
+          id="container"
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-12 gap-y-36"
+        >
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className="h-150 rounded-2xl flex flex-col gap-3 justify-between"
+            >
+              <div className="flex-1 rounded-2xl overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover"
+                />
               </div>
               <p className="text-2xl">{project.title}</p>
-            </div>;
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </div>
